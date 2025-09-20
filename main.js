41cc05c487dff736384e23962f65cf0adef6cae8
Jeyanth Arumugam <pooranajeyanthnathan.softsuave@gmail.com>
2025-09-20 14:10:50 +0530

    route struc

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ const PORT = 6969;
 app.get('/',(req,res)=>{
     res.json({msg:"Hello World"});
 });
+//CRUD
+app.get('/movies',()=>{
+
+})
+
+app.post('/movies',()=>{
+
+})
+
+app.put('/movies/:id',()=>{
+
+})
+
+app.delete('/movies/:id',()=>{
+
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
